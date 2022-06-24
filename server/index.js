edc7b663327383da7d42b8ad5fce15243a4d4ee1
MaxxM59 <87809496+MaxxM59@users.noreply.github.com>
2022-06-24 10:57:52 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,18 +8,22 @@ import userRouter from './routes/user.js';
 
 const app = express();
 
-app.use(express.json({limit: '30mb', extended: true}));
-app.use(express.urlencoded({limit: '30mb', extended: true}));
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
 app.use('/user', userRouter);
 
+app.get('/', (req, res) => {
+    res.send('APP IS RUNNING');
+});
+
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT;
 
 mongoose
-    .connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() =>
         app.listen(PORT, () =>
             console.log(`Server Running on Port: http://localhost:${PORT}`)
